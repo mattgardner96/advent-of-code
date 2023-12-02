4d98dd6d2a4a560f1315f86d5b97b2fc413d7619
Matt Gardner <39067505+mattgardner96@users.noreply.github.com>
2023-12-02 11:39:43 -0500

    part2 done

--- day2/day2.py
+++ day2/day2.py
@@ -41,9 +41,38 @@ def part1(lines):
     return(total_sum_line_nums)
 
 def part2(lines):
-    pass
+    print('part1')
+
+    power_sum = 0
+
+    for line_num,line in enumerate(lines):              # each game
+        hands = re.split(' |, |;|: ',line.strip())
+        hands.pop(0)                    # strip off "Game", we're iterating and it starts at row 0
+        hands.pop(0)                    # strip off line number
+        hands.remove('')                # clean data
+        
+        # create dictionary of red, green, and blue
+        colors_vals = {"red":0,"green":0,"blue":0}
+
+        power = 1
+
+        for color in colors_vals: # for each color
+            if color in hands:
+                for i,match in enumerate(hands): 
+                    if color == match:
+                        colors_vals[color] =  max(colors_vals[color],int(hands[i-1])) # this *should* contain the max of each color
+            power *= colors_vals[color]
+        print("colors_vals:",colors_vals,"power",power)
+
+        power_sum += power
+
+    return(power_sum)
+
+
 
 
 if __name__ == "__main__":
-    print(part1(lines))
+    # print(part1(lines))
+
+    print(part2(lines))
 
