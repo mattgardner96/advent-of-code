8c6f8275d9945a391c16de9001c80775f62329e2
Matt Gardner <39067505+mattgardner96@users.noreply.github.com>
2023-12-03 17:59:40 -0500

    traverse working, just have to add the answer

--- day3/day3.py
+++ day3/day3.py
@@ -4,23 +4,57 @@ from re import finditer
 os.chdir("/Users/mattgardner/Documents/advent-of-code/day3")
 
 # read in the input
-lines = open("input.txt","r").readlines()
+lines = open("test.txt","r").readlines()
 
 # create a list to store numbers and positions
 nums_array = list()
 
 def part1():
     find_numbers(lines,nums_array)
-    return pn_checker(lines,nums_array)
+    find_parts(lines,nums_array)
+    # return pn_checker(lines,nums_array)
 
 # find every number's position in the matrix
 def find_numbers(lines,nums_array):
     for line_num,this_line in enumerate(lines):
         for match in finditer(r'\d+', this_line):
-            nums_array.append([line_num, match.group(), match.span(), False])   # legend: [line_num, matched number, span, is_pn:default False]
+            nums_array.append([line_num, match.group(), list(match.span()), False])   # legend: [line_num, matched number, span, is_pn:default False]
+    for n in nums_array:
+        n[2][1] = n[2][1]-1 # make the end of the span the actual end of the number
 
-# pn checker (the hard part)
-def pn_checker(lines,nums_array):
+def find_parts(lines,nums_array):
+    for n in nums_array:
+
+        thislinenum = n[0]
+        
+        # create a grid to search
+        # we will traverse all the points in the grid. for example, for a three digit number somewhere inside array,
+        # there will 5 possible points above, 5 on the same row(three digits are the number), and five points below.
+        first_row = max(0,thislinenum - 1)
+        last_row = min(len(lines)-1,thislinenum + 1)        # need to check these rows inclusive
+        first_col = max(0,n[2][0]-1)
+        last_col = min(len(lines[last_row]) - 1, n[2][1]+1) # need to check these columns inclusive
+
+        # print(n[1],first_row,last_row,first_col,last_col)
+
+        n[3] = traverse(first_row,last_row,first_col,last_col)
+        print(n[1],n[3])
+
+def traverse(first_row,last_row,first_col,last_col) -> bool:
+    for row in lines[first_row:last_row+1]:
+        for char in row[first_col:last_col+1]:
+            if char not in '\n.0123456789': 
+                return(True) # found a real symbol in the zone we're checking, literally just end the search and return
+    # print("fell through! false")
+    return False
+
+if __name__ == "__main__":
+    part1()
+
+    # print("line",nums_array[-1][0],"number",nums_array[-1][1],"start+end",nums_array[-1][2],"is_pn",nums_array[-1][3])   # DEBUG
+
+''' def pn_checker(lines,nums_array):
+    pass
 
     sum_parts = 0;
     
@@ -70,10 +104,4 @@ def pn_checker(lines,nums_array):
         if num[3] == True: sum_parts += int(num[1])
 
 
-    return sum_parts
-
-
-if __name__ == "__main__":
-    print(part1())
-
-    # print("line",nums_array[-1][0],"number",nums_array[-1][1],"start+end",nums_array[-1][2],"is_pn",nums_array[-1][3])   # DEBUG
+    return sum_parts '''
