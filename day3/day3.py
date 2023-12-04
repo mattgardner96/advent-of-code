f0c17de7a8452c57bcd82ac9d41bd4013b76b3d7
Matt Gardner <39067505+mattgardner96@users.noreply.github.com>
2023-12-03 19:09:12 -0500

    made gear list with all gear locations

--- day3/day3.py
+++ day3/day3.py
@@ -9,7 +9,7 @@ lines = open("input.txt","r").readlines()
 # create a list to store numbers and positions
 nums_array = list() # usage: line number, number itself, start & end+1 locations, and is_part status
 
-stars = set() # if the value we detect is a star, 
+gears = dict() # key is an row-col tuple of the gear's location, value is a list of the nums for which that gear is adjacent
 
 count = 0
 
@@ -29,27 +29,25 @@ def find_numbers(lines,nums_array):
 def find_parts(lines,nums_array):
     total_sum = 0
 
-    for n in nums_array:
+    for i,n in enumerate(nums_array):
 
         thislinenum = n[0]
         
         # create a grid to search
-        # we will traverse all the points in the grid. for example, for a three digit number somewhere inside array,
+        # we will traverse all the points in the grid. for example, for a three digit number not on an array edge,
         # there will 5 possible points above, 5 on the same row(three digits are the number), and five points below.
         first_row = max(0,thislinenum - 1)
         last_row = min(len(lines)-1,thislinenum + 1)        # need to check these rows inclusive
         first_col = max(0,n[2][0]-1)
         last_col = min(len(lines[last_row]) - 1, n[2][1]+1) # need to check these columns inclusive
 
-        # print(n[1],first_row,last_row,first_col,last_col)
-
-        n[3] = traverse(first_row,last_row,first_col,last_col)
+        n[3] = traverse(first_row,last_row,first_col,last_col,i) # PART2: push along the index in nums_array so traverse can add that number to the set
 
         if n[3] == True: total_sum += int(n[1])
 
     return total_sum
 
-def traverse(first_row,last_row,first_col,last_col) -> bool:
+def traverse(first_row,last_row,first_col,last_col,i) -> bool:
     result = False
     global count
 
@@ -57,19 +55,28 @@ def traverse(first_row,last_row,first_col,last_col) -> bool:
         for c,char in enumerate(row[first_col:last_col+1]):
             if char not in '\n.0123456789': 
                 result = True # found a real symbol in the zone we're checking, but also looking for stars...
-                if char == "*":
-                    stars.add((first_row+r,first_col+c))
+                if char == "*": # if it's adjacent to another number, let's call it a gear
+                    key_tuple=(first_row+r, first_col+c)
+
+                    if key_tuple not in gears:
+                        gears[key_tuple] = [] # make the list if it doesn't exist
+                    gears[key_tuple].append(int(nums_array[i][1])) # add the number we're checking with to the gears dictionary
 
     return result
 
 def part2():
-    print(stars)
+    # for k,v in gears.items():
+    #     print(k,v)
+
+    
+
+
     pass
     # if any two numbers have an adjacent star, we have to multiply the numbers.
     # when we do part 1, log the row,col of any star and the 
     # for that star, add the 
 
 if __name__ == "__main__":
-    print("part1",part1())
+    print("part1 solution",part1())
     print(part2())
     # part2()
