c7c3c60d9532a6f615b99f57933070c7b58fe92c
Matt Gardner <39067505+mattgardner96@users.noreply.github.com>
2023-12-04 20:16:41 -0500

    refactored part1

--- day4/day4.py
+++ day4/day4.py
@@ -7,7 +7,7 @@ os.chdir("/Users/mattgardner/Documents/advent-of-code/day4")
 # read in the input
 lines = open("input.txt","r").readlines()
 
-cards = list() # usage: card number, list of winning numbers, list of your numbers, score of the matches
+cards = list() # usage: card number, list of winning numbers, list of your numbers, matches_count
 
 total_sum = 0
 
@@ -17,38 +17,42 @@ def ingest(lines):
         splitup = re.split(':  |: | \| |\|  ',line.strip())
         cards.append([card_num,re.split('  | ',splitup[1]),re.split('  | ',splitup[2]),0]) # every card inits to zero matches until matches are computed
 
-def compute_score(cards):
+def compute_wins(cards):
     # do the matches
     for card in cards:
         winners = Counter(card[1])
         your_nums = Counter(card[2])
-        matches_count = sum((winners & your_nums).values())
-        # compute points
-        # one point for the first win, then 
-        if matches_count > 0:
-            if matches_count == 1:
-                card[3] = 1 # exactly one match
-            else:
-                card[3] = 2**(matches_count - 1) # doubling with more matches
-        else:
-            card[3] = 0
-        # print(card[0],card[3])
+        card[3] = sum((winners & your_nums).values())
         
-
 def part1(lines,cards):
     global total_sum
     ingest(lines)
-    compute_score(cards) # this already saves scores to the cards list
+    compute_wins(cards) # this already saves scores to the cards list
+
+    this_score = 0
+    # compute points
+    # one point for the first win, then 
+
     for card in cards:
-        total_sum += card[3]
+        if card[3] > 0:
+            if card[3] == 1:
+                this_score = 1 # exactly one match
+            else:
+                this_score = 2**(card[3] - 1) # doubling with more matches
+        else:
+            this_score = 0
+        
+        total_sum += this_score
 
     return(total_sum)
 
-def part2():
+def part2(cards):
     pass
+    cards_augmented = cards
+
 
 
 if __name__ == "__main__":
     print("part1",part1(lines,cards))
-    print("part2",part2())
+    # print("part2",part2())
 
